75e65963be5dfe5f3becdc5f7ec6dfcd6706167c
cernat-catalin <cernat.catallin@gmail.com>
2022-05-15 21:42:01 +0200

    Introduce Scanner

---
+++ src/main/java/ccs/jlox/Scanner.java
@@ -0,0 +1,215 @@
+package ccs.jlox;
+
+import static ccs.jlox.TokenType.AND;
+import static ccs.jlox.TokenType.BANG;
+import static ccs.jlox.TokenType.BANG_EQUAL;
+import static ccs.jlox.TokenType.CLASS;
+import static ccs.jlox.TokenType.COMMA;
+import static ccs.jlox.TokenType.DOT;
+import static ccs.jlox.TokenType.ELSE;
+import static ccs.jlox.TokenType.EOF;
+import static ccs.jlox.TokenType.EQUAL;
+import static ccs.jlox.TokenType.EQUAL_EQUAL;
+import static ccs.jlox.TokenType.FALSE;
+import static ccs.jlox.TokenType.FOR;
+import static ccs.jlox.TokenType.FUN;
+import static ccs.jlox.TokenType.GREATER;
+import static ccs.jlox.TokenType.GREATER_EQUAL;
+import static ccs.jlox.TokenType.IDENTIFIER;
+import static ccs.jlox.TokenType.IF;
+import static ccs.jlox.TokenType.LEFT_BRACE;
+import static ccs.jlox.TokenType.LEFT_PAREN;
+import static ccs.jlox.TokenType.LESS;
+import static ccs.jlox.TokenType.LESS_EQUAL;
+import static ccs.jlox.TokenType.MINUS;
+import static ccs.jlox.TokenType.NIL;
+import static ccs.jlox.TokenType.NUMBER;
+import static ccs.jlox.TokenType.OR;
+import static ccs.jlox.TokenType.PLUS;
+import static ccs.jlox.TokenType.PRINT;
+import static ccs.jlox.TokenType.RETURN;
+import static ccs.jlox.TokenType.RIGHT_BRACE;
+import static ccs.jlox.TokenType.RIGHT_PAREN;
+import static ccs.jlox.TokenType.SEMICOLON;
+import static ccs.jlox.TokenType.SLASH;
+import static ccs.jlox.TokenType.STAR;
+import static ccs.jlox.TokenType.STRING;
+import static ccs.jlox.TokenType.SUPER;
+import static ccs.jlox.TokenType.THIS;
+import static ccs.jlox.TokenType.TRUE;
+import static ccs.jlox.TokenType.VAR;
+import static ccs.jlox.TokenType.WHILE;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Scanner {
+  private static final Map<String, TokenType> keywords;
+
+  static {
+    keywords = new HashMap<>();
+    keywords.put("and",    AND);
+    keywords.put("class",  CLASS);
+    keywords.put("else",   ELSE);
+    keywords.put("false",  FALSE);
+    keywords.put("for",    FOR);
+    keywords.put("fun",    FUN);
+    keywords.put("if",     IF);
+    keywords.put("nil",    NIL);
+    keywords.put("or",     OR);
+    keywords.put("print",  PRINT);
+    keywords.put("return", RETURN);
+    keywords.put("super",  SUPER);
+    keywords.put("this",   THIS);
+    keywords.put("true",   TRUE);
+    keywords.put("var",    VAR);
+    keywords.put("while",  WHILE);
+  }
+
+  private final String source;
+  private final List<Token> tokens = new ArrayList<>();
+  private int start = 0;
+  private int current = 0;
+  private int line = 1;
+
+  public Scanner(String source) {
+    this.source = source;
+  }
+
+  List<Token> scanTokens() {
+    while (!isAtEnd()) {
+      // We are at the beginning of the next lexeme.
+      start = current;
+      scanToken();
+    }
+    tokens.add(new Token(EOF, "", null, line));
+    return tokens;
+  }
+
+  private boolean isAtEnd() {
+    return current >= source.length();
+  }
+
+  private void scanToken() {
+    char c = advance();
+    switch (c) {
+      case '(' -> addToken(LEFT_PAREN);
+      case ')' -> addToken(RIGHT_PAREN);
+      case '{' -> addToken(LEFT_BRACE);
+      case '}' -> addToken(RIGHT_BRACE);
+      case ',' -> addToken(COMMA);
+      case '.' -> addToken(DOT);
+      case '-' -> addToken(MINUS);
+      case '+' -> addToken(PLUS);
+      case ';' -> addToken(SEMICOLON);
+      case '*' -> addToken(STAR);
+      case '!' -> addToken(match('=') ? BANG_EQUAL : BANG);
+      case '=' -> addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+      case '<' -> addToken(match('=') ? LESS_EQUAL : LESS);
+      case '>' -> addToken(match('=') ? GREATER_EQUAL : GREATER);
+      case '/' -> {
+        if (match('/')) {
+          // A comment goes until the end of the line.
+          while (peek() != '\n' && !isAtEnd()) advance();
+        } else {
+          addToken(SLASH);
+        }
+      }
+      case ' ', '\r', '\t' -> {}
+      case '\n' -> line++;
+      case '"' -> string();
+      default -> {
+        if (isDigit(c)) {
+          number();
+        } else if (isAlpha(c)) {
+          identifier();
+        }
+        Lox.error(line, "Unexpected character.");
+      }
+    }
+  }
+
+  private void identifier() {
+    while (isAlphaNumeric(peek())) advance();
+    String text = source.substring(start, current);
+    TokenType type = keywords.get(text);
+    if (type == null) type = IDENTIFIER;
+    addToken(type);
+  }
+
+  private void string() {
+    while (peek() != '"' && !isAtEnd()) {
+      if (peek() == '\n') line++;
+      advance();
+    }
+
+    if (isAtEnd()) {
+      Lox.error(line, "Unterminated string.");
+      return;
+    }
+
+    advance();
+
+    String value = source.substring(start + 1, current - 1);
+    addToken(STRING, value);
+  }
+
+  private char peek() {
+    if (isAtEnd()) return '\0';
+    return source.charAt(current);
+  }
+
+  private char peekNext() {
+    if (current + 1 >= source.length()) return '\0';
+    return source.charAt(current + 1);
+  }
+
+  private boolean match(char expected) {
+    if (isAtEnd()) return false;
+    if (source.charAt(current) != expected) return false;
+    current++;
+    return true;
+  }
+
+  private char advance() {
+    current++;
+    return source.charAt(current - 1);
+  }
+
+  private boolean isAlphaNumeric(char c) {
+    return isAlpha(c) || isDigit(c);
+  }
+
+  private boolean isAlpha(char c) {
+    return (c >= 'a' && c <= 'z') ||
+        (c >= 'A' && c <= 'Z') ||
+        c == '_';
+  }
+
+  private boolean isDigit(char c) {
+    return c >= '0' && c <= '9';
+  }
+
+  private void number() {
+    while (isDigit(peek())) advance();
+    // Look for a fractional part.
+    if (peek() == '.' && isDigit(peekNext())) {
+      // Consume the "."
+      advance();
+      while (isDigit(peek())) advance();
+    }
+    addToken(NUMBER,
+        Double.parseDouble(source.substring(start, current)));
+  }
+
+  private void addToken(TokenType type) {
+    addToken(type, null);
+  }
+
+  private void addToken(TokenType type, Object literal) {
+    String text = source.substring(start, current);
+    tokens.add(new Token(type, text, literal, line));
+  }
+}
---
+++ src/main/java/ccs/jlox/Token.java
@@ -0,0 +1,4 @@
+package ccs.jlox;
+
+public record Token(TokenType type, String lexeme, Object literal, int line) {
+}
